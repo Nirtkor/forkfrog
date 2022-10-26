867078671e0485caa4c818cf8d580b5fbfc7945b
Nirtkor <itnirtkor@gmail.com>
2022-10-26 14:06:22 +0300

    frog

---
+++ cs_2610.py
@@ -0,0 +1,17 @@
+from ctypes import string_at
+
+
+def counter_1(strinu):
+    stu = strinu.lower()
+    count = 0
+    for i in set(stu):
+        if stu.count(i) > 1:
+            count += 1
+    return count
+
+
+def counter_2(s):
+    return len([ch for ch in (set(s.lower())) if s.lower().count(ch) > 1])
+
+print(counter_1("aaabbbc"))
+print(counter_2("aaabbbc"))
